90511991527c8ed56599c91a398556bc4f1b281a
Hermelien <gau.hermelien2@gmail.com>
2025-07-15 15:36:56 +0200

    Chosing the opening book and playing a move

--- src/opening_book_explorer/main.py
+++ src/opening_book_explorer/main.py
@@ -1,5 +1,6 @@
 import sqlite3
 import chess
+
 liste_fichier=["anti-siciliennes.sqlite","Debut_Reti.sqlite", "KIA.sqlite", "Rossolimo.sqlite", "Semi-slave.sqlite","Sicilienne Dragon.sqlite"]
 def ask_opening():
     global c
@@ -10,27 +11,38 @@ def ask_opening():
     return c
     #position de départ echiquier fen : rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -
 
-def find_fen():
-    c=ask_opening()
-    current_fen="r1bqk1nr/pp2ppbp/2np2p1/2p5/4P3/2NP2P1/PPP2PBP/R1BQK1NR w KQkq -"
-    conn = sqlite3.connect(rf"..\..\openings\{liste_fichier[int(c)-1]}")  
-    cur = conn.cursor()
-    req=("SELECT * FROM positions") 
+def find_moves_from_fen(fen):
+    req=("SELECT * FROM positions where fromfen = '"+fen+"'") 
     result=cur.execute(req)
     liste_coups = []
     rows = result.fetchall()
+    liste_coups = []
     for row in rows:
-        if row[1]==current_fen:
-            liste_coups.append(row[2])
-    for i in range(len(liste_coups)):
-        print(f"coup possible : {i} --> {liste_coups[i]}")
-    choix = input("Choisir un coup à jouer : ")
+        liste_coups.append(row[2])
+    return liste_coups
+
+
+# MAIN 
+
+c=ask_opening()
+conn = sqlite3.connect(rf"..\..\openings\{liste_fichier[int(c)-1]}")  
+cur = conn.cursor()
+
+current_fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -"
+board = chess.Board(current_fen)
+moves = ['something to enter while']
+while moves:
+    moves = find_moves_from_fen(current_fen)
+    for i in range(len(moves)):
+        print(f"coup possible : {i} --> {moves[i]}")
+    choix = int(input("Choisir un coup à jouer : "))
+    board.push_san(moves[choix])
+    current_fen = board.fen()
     #test pour retrouver une fen depuis un coup
     # for row in rows:
     #     if row[2]==liste_coups[int(choix)]:
     #         print(row[1])
     #             #fen_suivante=rows[1]
-
     #test pour jouer coup
     # board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -")
     # print(board)
